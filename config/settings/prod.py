e9bd9ec3617e8ab218ba525550a6b2e80080dc32
박의진 <show400035@naver.com>
2021-03-19 03:22:41 +0900

    static_root 생성

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -5,3 +5,7 @@ ALLOWED_HOSTS = ['13.209.232.166']
 """
 prod.py 파일에는 서버 환경에 맞게끔 ALLOWED_HOSTS 항목에 서버의 고정 아이피를 등록하였다
 """
+STATIC_ROOT = BASE_DIR / 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
